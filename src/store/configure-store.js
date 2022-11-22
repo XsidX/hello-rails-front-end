ac28171cdd6d5e7f797cb71f9b03c823f75d8b8a
XsidX <developer.sid@outlook.com>
2022-11-22 16:21:44 +0300

    Add eslint and fix linter errors

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -2,9 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import greetingReducer from './greeting_slice';
 
 const store = configureStore({
-    reducer: {
-        greeting: greetingReducer,
-    }
+  reducer: {
+    greeting: greetingReducer,
+  },
 });
 
 export default store;
--- src/store/greeting_slice.js
+++ src/store/greeting_slice.js
@@ -1,19 +1,17 @@
-import { createSlice }  from  '@reduxjs/toolkit' ;
+import { createSlice } from '@reduxjs/toolkit';
 import getGreeting from './actions';
 
 const initialState = {
-    greeting: null,
-}
+  greeting: null,
+};
 
 const greetingSlice = createSlice({
-    name: 'greeting',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(getGreeting.fulfilled, (state, action) => {
-            state.greeting = action.payload.text;
-        });
-    }
+  name: 'greeting',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getGreeting.fulfilled, (state, action) => ({ ...state, greeting: action.payload.text })); // eslint-disable-line max-len
+  },
 });
 
 export default greetingSlice.reducer;
